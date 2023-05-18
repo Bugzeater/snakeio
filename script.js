27cb06878301931051ce7b3259ae552406d84263
Pasindu Madushan <128099179+Pasicodepro@users.noreply.github.com>
2023-05-18 22:29:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,88 @@
+const playBoard = document.querySelector(".playboard");
+const scoreElement = document.querySelector(".score");
+const highScoreElement = document.querySelector(".highscore");
+const controls = document.querySelectorAll(".controls h3")
+
+let gameOver = false;
+let foodX, foodY;
+let snakeX = 5, snakeY = 10;
+let snakeBody = [];
+let volocityX = 0, volocityY = 0;
+let setIntervalId;
+let score = 0;
+
+let highScore = localStorage.getItem("highscore") || 0;
+highScoreElement.innerText = `High - Score: ${highScore}`;
+
+const changeFoodPosition = () =>{
+    foodX = Math.floor(Math.random() * 30) + 1;
+    foodY = Math.floor(Math.random() * 30) + 1;
+}
+
+const handleGameOver = () =>{
+    clearInterval(setIntervalId);
+    alert("Game Over! Restart the game..");
+    location.reload();
+}
+
+const changeDirection = (e) =>{
+    if(e.key === "ArrowUp" && volocityY != 1){
+        volocityX = 0;
+        volocityY = -1;
+    }else if(e.key === "ArrowDown" && volocityY != -1){
+        volocityX = 0;
+        volocityY = 1;
+    }else if(e.key === "ArrowLeft" && volocityX != 1){
+        volocityX = -1;
+        volocityY = 0;
+    }else if(e.key === "ArrowRight" && volocityX != -1){
+        volocityX = 1;
+        volocityY = 0;
+    }
+}
+
+controls.forEach(key => {
+    key.addEventListener("click", () => changeDirection({ key: key.dataset.key}));
+})
+
+const initGame = () => {
+    if(gameOver) return handleGameOver();
+    let htmlMarkup = `<div class="food" style="grid-area : ${foodY} / ${foodX} "></div>`;
+
+    if (snakeX === foodX && snakeY === foodY) {
+        changeFoodPosition();
+        snakeBody.push([foodX, foodY]);
+        score++;
+        highScore = score >= highScore ? score : highScore;
+        localStorage.setItem("highscore", highScore);
+        scoreElement.innerText = `Score : ${score}`;
+        highScoreElement.innerText = `High - Score: ${highScore}`;
+    }
+
+    for (let i = snakeBody.length - 1; i > 0; i--){
+        snakeBody[i] = snakeBody[i - 1];    
+    }
+
+    snakeBody[0] = [snakeX, snakeY];
+
+    snakeX += volocityX;
+    snakeY += volocityY;
+
+    if(snakeX <= 0 || snakeX > 30 || snakeY <= 0 || snakeY > 30){
+        gameOver = true;
+    }
+
+    for (let i = 0; i < snakeBody.length; i++) {
+        htmlMarkup += `<div class="head" style="grid-area : ${snakeBody[i][1]} / ${snakeBody[i][0]} "></div>`;  
+        if(i !== 0 && snakeBody[0][1] === snakeBody[i][1] && snakeBody[0][0] === snakeBody[i][0]){
+            gameOver = true;
+        }
+    }
+
+    playBoard.innerHTML = htmlMarkup;
+}
+
+changeFoodPosition();
+setIntervalId = setInterval(initGame, 125);
+
+document.addEventListener("keydown", changeDirection);
